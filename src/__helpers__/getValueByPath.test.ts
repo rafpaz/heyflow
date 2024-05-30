4c65f62dfe6b1d97d1fb3695e2bd66230d9c6197
Rafael Paz <rafael@collato.com>
2024-05-30 17:04:37 +0200

    fix: typecheck

--- src/__helpers__/getValueByPath.test.ts
+++ src/__helpers__/getValueByPath.test.ts
@@ -1,4 +1,4 @@
-import { getPropertyValue } from './getValueByPath';
+import { getValueByPath } from '@/__helpers__/getValueByPath';
 
 describe('getPropertyValue', () => {
   const obj = {
@@ -21,50 +21,50 @@ describe('getPropertyValue', () => {
   };
 
   test('should return the value at a nested path', () => {
-    expect(getPropertyValue(obj, 'a.e')).toBe('f');
+    expect(getValueByPath(obj, 'a.e')).toBe('f');
   });
 
   test('should return undefined for non-existing path', () => {
-    expect(getPropertyValue(obj, 'a.b.e')).toBeUndefined();
+    expect(getValueByPath(obj, 'a.b.e')).toBeUndefined();
   });
 
   test('should return the value for a top-level path', () => {
-    expect(getPropertyValue(obj, 'x')).toBe(10);
+    expect(getValueByPath(obj, 'x')).toBe(10);
   });
 
   test('should return undefined for non-existing top-level path', () => {
-    expect(getPropertyValue(obj, 'w')).toBeUndefined();
+    expect(getValueByPath(obj, 'w')).toBeUndefined();
   });
 
   test('should return the value for a path with null value', () => {
-    expect(getPropertyValue(obj, 'y')).toBeNull();
+    expect(getValueByPath(obj, 'y')).toBeNull();
   });
 
   test('should return the value for a path pointing to an array', () => {
-    expect(getPropertyValue(obj, 'z')).toEqual([1, 2, 3]);
+    expect(getValueByPath(obj, 'z')).toEqual([1, 2, 3]);
   });
 
   test('should handle empty object', () => {
-    expect(getPropertyValue({}, 'a')).toBeUndefined();
+    expect(getValueByPath({}, 'a')).toBeUndefined();
   });
 
   test('should handle empty path', () => {
-    expect(getPropertyValue(obj, '')).toEqual(undefined);
+    expect(getValueByPath(obj, '')).toEqual(undefined);
   });
 
   test('should return undefined for invalid paths', () => {
-    expect(getPropertyValue(obj, 'a..b')).toBeUndefined();
-    expect(getPropertyValue(obj, 'a.b.')).toBeUndefined();
-    expect(getPropertyValue(obj, '.a.b')).toBeUndefined();
+    expect(getValueByPath(obj, 'a..b')).toBeUndefined();
+    expect(getValueByPath(obj, 'a.b.')).toBeUndefined();
+    expect(getValueByPath(obj, '.a.b')).toBeUndefined();
   });
 
   test('should handle paths with numbers as keys in objects', () => {
     const numKeyObj = { '1': 'one', '2': { '3': 'three' } };
-    expect(getPropertyValue(numKeyObj, '2.3')).toBe('three');
+    expect(getValueByPath(numKeyObj, '2.3')).toBe('three');
   });
 
   test('should handle paths with array indeices', () => {
-    expect(getPropertyValue(obj, 'a.b[0].c')).toBe(42);
-    expect(getPropertyValue(obj, 'a.b[1].d')).toBe('world');
+    expect(getValueByPath(obj, 'a.b[0].c')).toBe(42);
+    expect(getValueByPath(obj, 'a.b[1].d')).toBe('world');
   });
 });
