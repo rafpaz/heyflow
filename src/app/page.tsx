75d6067b267207ac95d48aa55c22b0312a88583d
Rafael Paz <rafael@collato.com>
2024-05-30 16:36:31 +0200

    feat: main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
 'use client';
 
+import Link from 'next/link';
 import * as React from 'react';
 import '@/lib/env';
 
-import JsonExplorer from '@/app/JsonExplorer/page';
-
 const App = () => {
   return (
-    <div className='p-4'>
-      <h1 className='text-2xl font-bold mb-4'>JSON Data Display</h1>
-      <JsonExplorer />
+    <div className='flex items-center justify-center min-h-screen flex-col'>
+      <h1 className='text-2xl font-bold mb-4'>Heyflow Task</h1>
+      <div className='flex flex-col items-start'>
+        <Link href='/jsonExplorer' className='text-blue-500 hover:underline'>
+          1. JSON Explorer
+        </Link>
+        <Link href='/customCheckbox' className='text-blue-500 hover:underline'>
+          2. Custom Checkbox
+        </Link>
+      </div>
     </div>
   );
 };
