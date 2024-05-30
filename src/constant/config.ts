28e23497217b090020f730fe7d73943ae6298423
Rafael Paz <rafael@collato.com>
2024-05-30 16:47:49 +0200

    chore: update config url

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,6 +1,5 @@
 export const siteConfig = {
   title: 'Heyflow Home Task',
   description: 'Heyflow Home Task',
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+  url: 'https://heyflow.vercel.app',
 };
